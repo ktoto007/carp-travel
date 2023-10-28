341f90ccefe18ee34fa55253a66de29f92e403a7
Streltsov Valerii <st.valera04@gmail.com>
2023-10-29 01:20:20 +0300

    fix nextconfig

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
+  trailingSlash: true,
+  output: "export",
 };
 
 module.exports = nextConfig;
